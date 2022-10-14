f2a48447e7a7ca2e8b6c62f35f8aa072f070f3c5
Basstrwp <113402668+Basstrwp@users.noreply.github.com>
2022-10-14 12:32:39 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,18 +10,18 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285808936396',
+  atasnama : 'Nishikigi Chisato'
 }
 
 // Other
 global.owner = ['6283102650464',"6282135724604"]
 global.premium = ['6283102650464']
 global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
-global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.sponsor = 'https://shop.tokotalk.com/chaosgamingidstores/home'
+global.packname = 'Elaina-Bot'
+global.footer = '© 2022 Nishikigi Chisato'
+global.author = 'Owner : Nishikigi Chisato\nIg1 : @me_kyluesky'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
@@ -38,7 +38,7 @@ global.act = {
 }
 
 global.thumb = fs.readFileSync('./lib/crizzy.jpg')
-global.crizzy = { url: "https://g.top4top.io/p_2443v88g61.jpg"}
+global.crizzy = { url: "https://telegra.ph/file/248851e2d6dbd7cda88e3.jpg"}
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
